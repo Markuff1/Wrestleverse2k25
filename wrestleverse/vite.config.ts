8f8420539ac842eaf7d671f3648fcb41dc4f2b03
Markuff1 <marknixon1@outlook.com>
2025-05-11 12:20:10 +0100

    Added items to push to live

--- wrestleverse/vite.config.ts
+++ wrestleverse/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Root path for custom domain (not a subdirectory)
   plugins: [react()],
-})
+});
